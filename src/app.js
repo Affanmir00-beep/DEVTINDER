a416ca7b3cca17416d8d7ac8078b5b0689e5579a
Affan Ahmad Mir <fa3176962@gmail.com>
2026-08-16 08:12:08 -0700

    get api feed api mongoose function

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const User = require("./models/user");
 
 
 
+
 app.use(express.json());
 
 app.post("/signup",async(req,res)=>{
@@ -26,10 +27,55 @@ app.post("/signup",async(req,res)=>{
     catch(err){
 res.status(400).send("bad request");
     }
-// }
-      res.send("data has been sent")
 });
 
+
+// api to find one user by email
+app.get("/user", async (req, res) => {
+
+    // const useremail=req.body.emailid;
+  
+    try{
+        const user= await User.findOne({age:22});
+     
+        if(!user){
+            res.send("user dont exist")
+        }
+        else{
+   res.send(user);
+        }
+    }catch(err){
+        res.status(404).send("err finding user",err)
+    }
+//     const useremail = req.body.emailid;
+//     // const Age=req.body.age;
+//     try {
+//         const user = await User.find({emailid:useremail});
+//         // const user1= await User.find({age:Age})
+//          if(user.length===0){
+//             res.status(404).send("user not found");
+//         }
+//          else{
+//    res.status(200).send(user);
+//     console.log(user);
+//          }
+//     } catch (err) {
+//         console.log("err found", err);
+//         return res.status(500).json({ message: "server error" });
+//     }
+});
+// now we are creating feed api the job of this feed api is to get all the suers from databse and show it
+app.get("/feed", async(req,res)=>{
+    // now we have to get data from database
+    // const allusers=req.body;
+    try{
+    const alluserss=await User.find({});
+    res.send(alluserss);
+    }
+    catch(err){
+        res.status(404).send(err,"err is happeninging")
+    }
+});
 connectDB().then(()=>{ 
     console.log("connected to the database");
 app.listen(3000,()=>{
