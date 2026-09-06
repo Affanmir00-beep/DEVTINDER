058dd67ecfa5cb50fadf66356dcb0aac413c070d
Affan Ahmad Mir <fa3176962@gmail.com>
2026-09-06 11:12:07 -0700

    added user auth middle ware  amde sendconnection  api expires cookies and toekn

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app=express();
 const connectDB=require("./config/database");
 const User = require("./models/user");
 const { vlaidatesignupdata } = require("./utils/validations.js");
-const bcrypt=require('bcrypt')
-const cookieparser=require('cookie-parser')
-const jwt=require('jsonwebtoken')
-
+const bcrypt=require('bcrypt');
+const cookieparser=require('cookie-parser');
+const jwt=require('jsonwebtoken');
+const {userauth}=require("./middlewares/auth.js");
 
 app.use(express.json()); 
 app.use(cookieparser());
@@ -41,7 +41,7 @@ app.post("/signup",async(req,res)=>{
 res.status(400).send(err.message)
     }
 });
-// NOW  WEA RE CREATING LOGIN API
+// NOW  WE  ARE CREATING LOGIN API
 app.post("/login", async(req,res)=>{
     try{
 const {emailid,password}=req.body;
@@ -53,10 +53,13 @@ const {emailid,password}=req.body;
 const ispasswordcorrect = await bcrypt.compare(password, user.password);
 if(ispasswordcorrect){
     // create a jwt token
-    const token= await jwt.sign({_id:user._id},"Aff@n123&")
+    const token= await jwt.sign({_id:user._id},"Aff@n123&",{expiresIn:"1d"});
     console.log(token);
     //  add jwt token to cookie and send the response back tot the user
-    res.cookie("token", token);
+    res.cookie("token", token, {
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        // httpOnly: true
+    });
     res.send("login successfull");
 }
 else{
@@ -67,137 +70,31 @@ else{
     res.status(400).send(err.message)
     }
 });
-// get api to fetch  profile and suthenticate how cookie trvael with every req
-app.get("/profile", async(req,res)=>{
-try{
-        const cookies=req.cookies;
-    // console.log(cookies);
-    const {token}=cookies;
-    if(!token){
-        throw new Error("invalid token");
-    }
- const decodedmsg=await jwt.verify(token,"Aff@n123&");
+app.post("/sendconnectionrequest",userauth,async(req,res)=>{
 
- const {_id}=decodedmsg;
-
-//  console.log("logged user is"+ _id);
-
- const loggedinuser=await User.findById(_id);
- if(!loggedinuser){
-    throw new Error("user doesnot exist")
- }
- res.send(loggedinuser);
-//  console.log(decodedmsg);
-} 
-catch(err){
-res.status(400).send(err.message)
-}
+    const user=req.user;
+    console.log("sendin connection request");
+    res.send(user.firstname+" is sending connection request to "+req.body.to);
 })
-// api to find one user by email
-app.get("/user", async (req, res) => {
-
-    // const useremail=req.body.emailid;
-//   const Age=req.body.age;
-    try{
-        const user= await User.findOne({age:22});
-     
-        if(!user){
-            
-            res.send("user dont exist")
-        } 
-        else{
-   res.send(user);
-        }
-    }catch(err){
-        res.status(404).send("err finding user",err)
-    }
-//     const useremail = req.body.emailid;
-//     // const Age=req.body.age;
-//     try {
-//         const user = await User.find({emailid:useremail});
-//         // const user1= await User.find({age:Age})
-//          if(user.length===0){
-//             res.status(404).send("user not found");
-//         }
-//          else{
-//    res.status(200).send(user);
-//     console.log(user);
-//          }
-//     } catch (err) {
-//         console.log("err found", err);
-//         return res.status(500).json({ message: "server error" });
-//     }
-});
-// now we are creating feed api the job of this feed api is to get all the suers from databse and show it
-app.get("/feed", async(req,res)=>{
-    // now we have to get data from database
-    // const allusers=req.body;
-    try{
-    const alluserss=await User.find({});
-    res.send(alluserss);
-    }
-    catch(err){
-        res.status(404).send(err,"err is happeninging")
-    }
-});
-app.delete("/user",async(req,res)=>{
 
 
-     const userId=req.body.userId;
-
-    try{
-        // const userid=await User.findByIdAndDelete(_id:userId);
-        // (_id:userId)  = (userId) wrriiten in mongoose because in mongoose we have to pass the id of the user to delete the user from database. and in our case we are passing the userId from the request body. so we have to pass the userId to findByIdAndDelete method. and in mongoose we have to pass the id of the user to delete the user from database. and in our case we are passing the userId from the request body. so we have to pass the userId to findByIdAndDelete method.
-        //   const user=await User.findByIdAndDelete(userId);
-          const user=await User.findByIdAndDelete({_id:userId});
-        res.send("user deleted successfully");
-        console.log(user,"user deleted successfully");
-    }
-  catch(err){
-    console.log(err);
-    res.status(404).send("error deleting user",err);
-  }
-
-});
-
-app.patch("/user", async(req,res)=>{
 
 
-    // const userId=req.body.userId;
-    const useeremail=req.body.emailid;
-    const updatedata=req.body;
-    // const{ usreid,..updateddata}=req.body;
 
 
-    try{
-const Allowed_updates=[
-        "photourl","skills","about","age"
-    ];
-    const isupdateallowed=Object.keys(updatedata).every(k=>Allowed_updates.includes(k));
-  if (!isupdateallowed) {
-  throw new Error("cant update this info");
-}
-    //  const updateddata= await User.findOneAndUpdate({emailid:useeremail},updatedata,{returnDocument:"before", runValidators: true },);
-    const updateduser = await User.findByIdAndUpdate(
-  userid,
-  updatedata,
-  {
-    new: true,
-    runValidators: true,
-  });
-     res.send("user updated successfully");
-     console.log(updateddata,"user updated successfully");
-    }
-    catch(err){
-        res.status(400).send("error while updating  user"   +err.message)
-        console.log(err);
-    }
-});
+
+
+
+
+
+
+
+
 connectDB().then(()=>{ 
     console.log("connected to the database");
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
-});
+    app.listen(3000,()=>{
+        console.log("server is running on port 3000");
+    });
 }
 ).catch(err=>{
     console.log("error connecting to the database",err);
@@ -205,5 +102,134 @@ app.listen(3000,()=>{
 
 console.log("Connected state:", User.db.readyState);
 
+// all the api which we have amde before {
+
+// app.get("/user", async (req, res) => {
+
+//     // const useremail=req.body.emailid;
+// //   const Age=req.body.age;
+//     try{
+//         const user= await User.findOne({age:22});
+     
+//         if(!user){
+            
+//             res.send("user dont exist")
+//         } 
+//         else{
+//    res.send(user);
+//         }
+//     }catch(err){
+//         res.status(404).send("err finding user",err)
+//     }
+// //     const useremail = req.body.emailid;
+// //     // const Age=req.body.age;
+// //     try {
+// //         const user = await User.find({emailid:useremail});
+// //         // const user1= await User.find({age:Age})
+// //          if(user.length===0){
+// //             res.status(404).send("user not found");
+// //         }
+// //          else{
+// //    res.status(200).send(user);
+// //     console.log(user);
+// //          }
+// //     } catch (err) {
+// //         console.log("err found", err);
+// //         return res.status(500).json({ message: "server error" });
+// //     }
+// });
+// // now we are creating feed api the job of this feed api is to get all the suers from databse and show it
+// app.get("/feed", async(req,res)=>{
+//     // now we have to get data from database
+//     // const allusers=req.body;
+//     try{
+//     const alluserss=await User.find({});
+//     res.send(alluserss);
+//     }
+//     catch(err){
+//         res.status(404).send(err,"err is happeninging")
+//     }
+// });
+// app.delete("/user",async(req,res)=>{
 
 
+//      const userId=req.body.userId;
+
+//     try{
+//         // const userid=await User.findByIdAndDelete(_id:userId);
+//         // (_id:userId)  = (userId) wrriiten in mongoose because in mongoose we have to pass the id of the user to delete the user from database. and in our case we are passing the userId from the request body. so we have to pass the userId to findByIdAndDelete method. and in mongoose we have to pass the id of the user to delete the user from database. and in our case we are passing the userId from the request body. so we have to pass the userId to findByIdAndDelete method.
+//         //   const user=await User.findByIdAndDelete(userId);
+//           const user=await User.findByIdAndDelete({_id:userId});
+//         res.send("user deleted successfully");
+//         console.log(user,"user deleted successfully");
+//     }
+//   catch(err){
+//     console.log(err);
+//     res.status(404).send("error deleting user",err);
+//   }
+
+// });
+
+// app.patch("/user", async(req,res)=>{
+
+
+//     // const userId=req.body.userId;
+//     const useeremail=req.body.emailid;
+//     const updatedata=req.body;
+//     // const{ usreid,..updateddata}=req.body;
+
+
+//     try{
+// const Allowed_updates=[
+//         "photourl","skills","about","age"
+//     ];
+//     const isupdateallowed=Object.keys(updatedata).every(k=>Allowed_updates.includes(k));
+//   if (!isupdateallowed) {
+//   throw new Error("cant update this info");
+// }
+//     //  const updateddata= await User.findOneAndUpdate({emailid:useeremail},updatedata,{returnDocument:"before", runValidators: true },);
+//     const updateduser = await User.findByIdAndUpdate(
+//   userid,
+//   updatedata,
+//   {
+//     new: true,
+//     runValidators: true,
+//   });
+//      res.send("user updated successfully");
+//      console.log(updateddata,"user updated successfully");
+//     }
+//     catch(err){
+//         res.status(400).send("error while updating  user"   +err.message)
+//         console.log(err);
+//     }
+// });
+// get api to fetch  profile and suthenticate how cookie trvael with every req
+// app.get("/profile", userauth, async(req,res)=>{
+// try{
+// //         const cookies=req.cookies;
+// //     // console.log(cookies);
+// //     const {token}=cookies;
+// //     if(!token){
+// //         throw new Error("invalid token");
+// //     }
+// //  const decodedmsg=await jwt.verify(token,"Aff@n123&");
+
+// //  const {_id}=decodedmsg;
+
+// // //  console.log("logged user is"+ _id);
+
+// //  const loggedinuser=await User.findById(_id);
+// const loggedinuser=req.user;
+// // req.user is set in the userauth middleware after verifying the token and fetching the user from the database.
+//  if(!loggedinuser){
+//     throw new Error("user doesnot exist")
+//  }
+//  res.send(loggedinuser);
+// //  console.log(decodedmsg);
+// } 
+// catch(err){
+// res.status(400).send(err.message)
+// }
+// })
+// // api to find one user by email
+// }
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,29 +1,74 @@
-const authmiddleware=(req,res,next)=>{
-    console.log("hello from the admin route");
- const token="123456"; // this is just a dummy token for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
-    const isAuthorized=token==="123456"; // this is just a dummy logic for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
-     if(!isAuthorized){
-         res.status(401).send("Unauthorized request");
-        }else{
-        // res.send("All data sent");
-        next();
+const  jwt=require("jsonwebtoken")
+ const User=require("../models/user");
+// const authmiddleware=(req,res,next)=>{
+//     console.log("hello from the admin route");
+//  const token="123456"; // this is just a dummy token for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
+//     const isAuthorized=token==="123456"; // this is just a dummy logic for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
+//      if(!isAuthorized){
+//          res.status(401).send("Unauthorized request");
+//         }else{
+//         // res.send("All data sent");
+//         next();
+//     }
+// };
+// const userauth=(req,res,next)=>{
+//     console.log("user is trying to access the user route");
+//  const token="123456"; // this is just a dummy token for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
+//     const isAuthorized=token==="123456"; // this is just a dummy logic for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
+//      if(!isAuthorized){
+//          res.status(401).send("Unauthorized user");
+//         }else{
+//         // res.send("All data sent");
+//         next();
+//     }
+// };
+
+// module.exports={
+//     authmiddleware,
+//     userauth,
+// };
+
+const userauth= async(req,res,next)=>{
+ 
+ try{
+     // read the token from req cookies
+    // { // const  cookies=req.cookies;
+    // const {token}=cookies;}(also a valid way to write)
+    const {token}=req.cookies;
+    if(!token){
+        throw new Error("token not found");
     }
-};
-const userauth=(req,res,next)=>{
-    console.log("user is trying to access the user route");
- const token="123456"; // this is just a dummy token for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
-    const isAuthorized=token==="123456"; // this is just a dummy logic for checking if the request is authorized or not. in real world we will use some kind of authentication mechanism like JWT or OAuth to check if the request is authorized or not.
-     if(!isAuthorized){
-         res.status(401).send("Unauthorized user");
-        }else{
-        // res.send("All data sent");
-        next();
-    }
-};
+    // validate the token
+     const decodedobj= await jwt.verify(token,"Aff@n123&");
+     const {_id}=decodedobj;
+    //  find the user in the database
+    const user= await User.findById(_id);
+     if(!user){
+        throw new Error("user doesnot found");
+     }
+     req.user=user;
+     next();
+ }  
+ catch(err){
+    res.status(401).send("Error: " + err.message);
+ }
+// // Read the JWT from cookies.
+// Verify and decode it.
+// Extract the user ID.
+// Find that user in the database.
+// Store the user on req.
+// Call next() so the protected route continue
+
+    // find the username
+
+
+
+}
+
+
 module.exports={
-    authmiddleware,
-    userauth,
-};
+    userauth
+}
 
 
 
